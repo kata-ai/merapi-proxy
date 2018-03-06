e53b8f10c94d07fc78338ebe4f9bc3426fba9c9d
Yoga Aliarham <aliarham.skom@gmail.com>
2018-03-06 17:46:36 +0700

    Enable lazy loading for component type proxy (#1)
    * Enable lazy loading for component type proxy
    
    * Enable retry for lazy loading proxy component

--- async.js
+++ async.js
@@ -3,17 +3,63 @@ const proxy = require("./lib/proxy");
 const apiAsync = require("./lib/api_async");
 const request = require("request-promise");
 const url = require("url");
+const async = require("async");
+
+function lazyLoad(uri, opt, logger) {
+    let info;
+    let infoUri =  uri.protocol + "//" + uri.host + "/info";
+    let status = () => {
+        return info !== undefined;
+    };
+
+    let ready = false;
+    let obj = {
+        isReady: () => ready
+    };
+    logger.warn(`Attempting lazy connection to ${infoUri} ..`);
+    async.until(status, function(callback) {
+        request({
+            uri: infoUri,
+            headers: {
+                Authorization: "Bearer " + opt.secret
+            },
+            json: true
+        }, (err, res) => {
+            if (!err) {
+                info = res.body;
+                info.uri = uri.host;
+                info.secure = uri.protocol === "https:";
+            }
+        }).catch((err) => err);
+        setTimeout(function() {
+            callback(null, info);
+        }, opt.retryDelay || 3000);
+    }, (err, res) => {
+        if (!err) {
+            logger.info(`Connection success ${infoUri}`)
+            proxy(apiAsync, info, opt, logger, obj);
+            ready = true;
+        }
+    });
+
+    return obj;
+}
 
 module.exports = Promise.coroutine(function*(uri, options = {}, logger) {
-    uri = url.parse(uri); 
-    let info = yield request({
-        uri: uri.protocol + "//" + uri.host + "/info",
-        headers: {
-            Authorization: "Bearer " + options.secret
-        },
-        json: true
-    });
-    info.uri = uri.host;
-    info.secure = uri.protocol === "https:";
-    return proxy(apiAsync, info, options, logger);
+    uri = url.parse(uri);
+    if (options.lazy) {
+        return lazyLoad(uri, options, logger);
+    } else {
+        let info = yield request({
+            uri: uri.protocol + "//" + uri.host + "/info",
+            headers: {
+                Authorization: "Bearer " + options.secret
+            },
+            json: true
+        });
+        info.uri = uri.host;
+        info.secure = uri.protocol === "https:";
+        return proxy(apiAsync, info, options, logger);
+    }
+    
 });
